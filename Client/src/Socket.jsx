1cd289e5c21aceff86ab0eac00198c0f29874a1c
Himanshu116k <himanshu1991patna@gmail.com>
2025-09-06 20:27:34 +0530

    prd changes2

--- Client/src/Socket.jsx
+++ Client/src/Socket.jsx
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", { withCredentials: true });
+const socket = io(import.meta.env.VITE_BACKEND_URL, { withCredentials: true });
 export default socket;
